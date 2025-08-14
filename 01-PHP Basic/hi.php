1154b6242d4c48224e1c82204a931580aafd62ac
Pappu <pappuroy10102000@gmail.com>
2025-08-14 20:52:03 +0600

    PHP Variables

---
+++ 01-PHP Basic/hi.php
@@ -0,0 +1,7 @@
+<?php
+print "Hello PHP, \n";
+$h = "It's 2025";
+echo $h."\n";   // We can print newline through \n but it doesn't work in browser, so we use <br> in browser
+echo "Hello World!"."\n";  // We can use . for concate two output
+print "Pappu Roy is here...";
+?>
